7e75e1345ad3209c5541d11032a3ab7e01f898b2
Ismayil-13-Aliyev <aze.ismayil13@gmail.com>
2025-04-04 22:33:58 +0400

    start homework

---
+++ index.js
@@ -0,0 +1,15 @@
+const clickMeBtn = document.getElementById("click-me-btn");
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+
+let tasks = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  clickMeBtn.addEventListener("click", () => {
+    tasks.push(input.value);
+
+    const task = document.createElement("p");
+    task.innerText = input.value;
+    list.append(task);
+  });
+});
